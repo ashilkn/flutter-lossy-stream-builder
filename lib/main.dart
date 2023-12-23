d715bae139120f751aa24faef9c8b5275f7c6e92
ashilkn <ashilkn99@gmail.com>
2023-12-23 12:03:57 +0530

    Create StreamBuilder and an event for it which shows StreamBuilder missing to render event data when stream is emitting events every 8ms (120hz device)

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:custom_stream_builder/stream.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Stream<int>? stream;
+  final listOfWidgets = <Widget>[];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: StreamBuilder(
+                key: ValueKey(stream.hashCode),
+                stream: stream,
+                builder: ((context, snapshot) {
+                  if (snapshot.hasData) {
+                    listOfWidgets.add(Text(snapshot.data.toString()));
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: listOfWidgets,
+                    );
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                }))),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              stream =
+                  NumberGenrator(endOnTick: 10, const Duration(milliseconds: 8))
+                      .getStream;
+              listOfWidgets.clear();
+            });
+          },
+          child: const Icon(Icons.replay_outlined),
+        )
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+}
---
+++ lib/stream.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+
+class NumberGenrator {
+  NumberGenrator(Duration duration, {required int endOnTick}) {
+    Timer.periodic(duration, (timer) {
+      _controller.add(timer.tick);
+      if (endOnTick < timer.tick) {
+        timer.cancel();
+        _controller.close();
+      }
+    });
+  }
+
+  final _controller = StreamController<int>();
+
+  Stream<int> get getStream => _controller.stream;
+}
